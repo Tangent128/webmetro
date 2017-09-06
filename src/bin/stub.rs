9b3a6db07472bcd680f753740ee6b1bed99ee3d4
Tangent 128 <Tangent128@gmail.com>
2017-09-06 01:53:49 -0400

    fixup bin for reference refactor.

--- src/bin/stub.rs
+++ src/bin/stub.rs
@@ -6,23 +6,23 @@ use lab_ebml::webm::*;
 pub fn main() {
     let mut cursor = Cursor::new(Vec::new());
 
-    encode_webm_element(WebmElement::EbmlHead, &mut cursor).unwrap();
-    encode_webm_element(WebmElement::Segment, &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::EbmlHead, &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::Segment, &mut cursor).unwrap();
 
-    encode_webm_element(WebmElement::Tracks(&[]), &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::Tracks(&[]), &mut cursor).unwrap();
 
-    encode_webm_element(WebmElement::Cluster, &mut cursor).unwrap();
-    encode_webm_element(WebmElement::Timecode(0), &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::Cluster, &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::Timecode(0), &mut cursor).unwrap();
 
-    encode_webm_element(WebmElement::SimpleBlock(SimpleBlock {
+    encode_webm_element(&WebmElement::SimpleBlock(SimpleBlock {
         track: 3,
         flags: 0x0,
         timecode: 123,
         data: "Hello, World".as_bytes()
     }), &mut cursor).unwrap();
 
-    encode_webm_element(WebmElement::Cluster, &mut cursor).unwrap();
-    encode_webm_element(WebmElement::Timecode(1000), &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::Cluster, &mut cursor).unwrap();
+    encode_webm_element(&WebmElement::Timecode(1000), &mut cursor).unwrap();
 
     stdout().write_all(&cursor.get_ref()).unwrap();
 }
