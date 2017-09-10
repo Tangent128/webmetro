ef457287795b2259b748a63f9ca2b3b5c28ed534
Tangent 128 <Tangent128@gmail.com>
2017-09-10 19:44:54 -0400

    Have stub server recognize a URL path

--- src/bin/loop_server.rs
+++ src/bin/loop_server.rs
@@ -3,12 +3,12 @@ extern crate hyper;
 extern crate lab_ebml;
 
 use futures::future::FutureResult;
-use hyper::StatusCode;
+use hyper::{Get, StatusCode};
 use hyper::server::{Http, Request, Response, Service};
 use std::env::args;
 use std::net::ToSocketAddrs;
 
-const SRC_FILE: &'static [u8] = include_bytes!("../data/test1.webm");
+//const SRC_FILE: &'static [u8] = include_bytes!("../data/test1.webm");
 
 struct WebmServer;
 
@@ -18,7 +18,17 @@ impl Service for WebmServer {
     type Error = hyper::Error;
     type Future = FutureResult<Response, hyper::Error>;
     fn call(&self, req: Request) -> Self::Future {
-        futures::future::ok(Response::new().with_status(StatusCode::NotFound))
+        let response = match (req.method(), req.path()) {
+            (&Get, "/loop") => {
+                Response::new()
+                    .with_body("<Insert WebM stream here>")
+            },
+            _ => {
+                Response::new()
+                    .with_status(StatusCode::NotFound)
+            }
+        };
+        futures::future::ok(response)
     }
 }
 
