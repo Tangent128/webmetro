7485119028d079e11eabca66b2d88398277ed4e3
Tangent Wantwight <tangent128@AngelIsland.fios-router.home>
2019-11-16 14:22:53 -0500

    remove an unneeded test

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,6 @@ pub use crate::ebml::{EbmlError, FromEbml};
 
 #[cfg(test)]
 mod tests {
-    use futures::future::{ok, Future};
-
     pub const TEST_FILE: &'static [u8] = include_bytes!("data/test1.webm");
     pub const ENCODE_WEBM_TEST_FILE: &'static [u8] = include_bytes!("data/encode_webm_test.webm");
-
-    #[test]
-    fn hello_futures() {
-        let my_future = ok::<String, ()>("Hello".into())
-            .map(|hello| hello + ", Futures!");
-
-        let string_result = my_future.wait().unwrap();
-
-        assert_eq!(string_result, "Hello, Futures!");
-    }
 }
